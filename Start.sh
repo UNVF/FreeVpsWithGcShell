5ca369fd55958b9fbb6864653a34a25accc11ca0
UNV <139604784+UNVF@users.noreply.github.com>
2023-11-03 12:07:22 +0700

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Add chrome desktop
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+
+# Update package information
+sudo apt-get update
+
+# Install Google Chrome
+sudo dpkg -i chrome*
+
 # Install dbus-x11
 sudo apt install dbus-x11
 
 # Install XFCE and XFCE terminal
 sudo apt-get install xfce4 xfce4-terminal
-
-# Update package information
-sudo apt-get update
 
 # Install any pending dependencies
 sudo apt-get install -f
@@ -15,18 +21,12 @@ sudo apt-get install -f
 # Install Firefox ESR
 sudo apt install firefox-esr
 
-# Install Google Chrome
-sudo dpkg -i chrome*
-
 # Add VSCode repository
 sudo sh -c 'echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
 # Add Microsoft GPG key
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-
-# Add chrome desktop
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 
 # Update package information after adding new repository
 sudo apt-get update
@@ -38,4 +38,4 @@ sudo apt-get install code
 read -p "Enter the command you want to run: " USER_COMMAND
 
 # Running the user-provided command using CRD_TOKEN
-echo "$CRD_TOKEN" | sudo -S bash -c "$USER_COMMAND"￼Enter
+echo "$CRD_TOKEN" | bash -c "$USER_COMMAND"
